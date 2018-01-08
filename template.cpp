170af1a6dd24296fdbd612abd8365e86178dd2c1
Hiroyuki Sano <sh19910711@gmail.com>
2018-01-09 08:42:06 +0900

    add template.cpp

---
+++ template.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < n; ++i)
+typedef long long int_t;
+
+bool input() {
+  return false;
+}
+
+void solve() {
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  while (input()) {
+    solve();
+  }
+  return 0;
+}
